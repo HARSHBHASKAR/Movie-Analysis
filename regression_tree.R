fbad9cfed3c741325d9319d25282327f6cbf7496
Harshavardhan Bhaskar <55582080+HARSHBHASKAR@users.noreply.github.com>
2021-11-20 10:51:08 +0530

    Update - building and run
    building reg-tree model and running on train-set check mean squared error(mse) from predicted and response variable values.

--- regression_tree.R
+++ regression_tree.R
@@ -14,3 +14,21 @@ set.seed(0)
 split = sample.split(movie, SplitRatio = 0.8)
 train = subset(movie, split == TRUE)
 test = subset(movie, split == FALSE)
+
+#Building regression decision tree
+
+#installing reqd. packages
+install.packages("rpart")
+install.packages("rpart.plot")
+library(rpart)
+library(rpart.plot)
+
+# Run regression tree model on train-set
+regtree = rpart(formula = Collection~., data = train, control = rpart.control(maxdepth = 3))
+# plot tree
+rpart.plot(regtree,box.palette = "RdYlGn", digits = -3)
+#predict values at any point
+test$pred <- predict(regtree, test , type = "vector") # TYPE = "CLASS" FOR CLASSIFICATION TREES
+
+MSE2 = mean((test$pred - test$Collection)^2)
+
